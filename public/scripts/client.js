77c58dc2276d2d2347617c7877f0a56c927ed0e7
Yu Gao <gaoyububest@gmail.com>
2020-12-01 20:38:07 +0000

    updated new poll mandotory input fields

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -9,13 +9,21 @@ $(document).ready(() => {
         inputEmail: {
             required: true,
             email: true
-        }
+        },
+        inputNewQuestion: {
+          required: true,
+        },
+        inputOpt1: {
+          required: true,
+        },
+        inputOpt2: {
+          required: true,
+        },
     }
   });
 
-  $('div.emailValidation input').on('keyup blur', function () {
+  $('form.create-poll input').on('keyup blur', function () {
       if ($('form.create-poll').valid()) {
-
           $('button.newPollSubmitButton').prop('disabled', false);
       } else {
           $('button.newPollSubmitButton').prop('disabled', 'disabled');
