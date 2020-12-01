98fcf763c83eabda0c42ae12a6a4017799b68f17
Alberto Olvera Sotres <mty2yyz@gmail.com>
2020-11-30 23:34:09 -0500

    Updated result.js to render with query results.  Added view result.ejs

--- routes/result.js
+++ routes/result.js
@@ -3,7 +3,8 @@ const router  = express.Router({ mergeParams: true });
 
 module.exports = (db) => {
   router.get("/", (req, res) => {
-    const results = {};
+    const query1 = {};
+    const query2 = {};
     const url = req.params.id
     console.log("I'm at /my/:id", "id:", req.params.id);
     db.query(`
@@ -15,7 +16,7 @@ module.exports = (db) => {
         // const poll_result = data.rows;
         // console.log(poll_results);
         // res.json({ poll_result });
-        results.question = data.rows;
+        query1.question = data.rows[0].question;
         // res.json({ results });
       })
       .then(
@@ -30,10 +31,17 @@ module.exports = (db) => {
           [req.params.id]
           )
           .then(data => {
-            results.options = data.rows;
-            res.json({ results });
+            // console.log("data.rows[0] = ", data.rows[0]);
+            data.rows.forEach((element, index) => {
+              // console.log(element);
+              query2[index] = Object.values(element);
+            });
+            const templateVars = { question: query1.question, results: query2};
+            // res.json({ results });
+            console.log(templateVars);
+            // console.log(results);
+            res.render("result", templateVars);
           })
-
       .catch(err => {
         res
           .status(500)
