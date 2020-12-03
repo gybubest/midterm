bee3faa506613bfea2e0970f6839b890dbb94fe8
Alberto Olvera Sotres <mty2yyz@gmail.com>
2020-12-02 19:50:32 -0500

    Added top-choice for voter_name functionality

--- routes/result.js
+++ routes/result.js
@@ -1,28 +1,23 @@
 const express = require('express');
 const router  = express.Router({ mergeParams: true });
-const { getQuestion, getOptionsWithWeighting, getAllVotersFirstOption } = require('../lib/data-helpers');
+const { getQuestion, getOptionsWithPercentage, getAllVotersFirstOption } = require('../lib/data-helpers');
 
 module.exports = (db) => {
   router.get("/", (req, res) => {
-    Promise.all ([
-      getQuestion(db, res, req.params.id),
-      getOptionsWithWeighting(db, res, req.params.id)
+    Promise.all([
+      getQuestion(db, req.params.id),
+      getOptionsWithPercentage(db, req.params.id),
+      getAllVotersFirstOption(db, req.params.id)
     ])
-    .then( ([questionQuery, optionsQuery]) => {
-      // if (getOptionsWithWeighting.error) {
-      //   const templateVars {}
-      // }
-
-      console.log("***questionQuery =", questionQuery);
-      const templateVars = { question: questionQuery, results: optionsQuery};
-      console.log("templateVars =", templateVars);
-      res.render("result", templateVars);
-    })
-    .catch(err => {
-      res
-        .status(500)
-        .json({ error: err.stack })
-    });
+      .then(([questionQuery, optionsQuery, votersFirstOption]) => {
+        const templateVars = { question: questionQuery, results: optionsQuery, firstOption: votersFirstOption };
+        res.render("result", templateVars);
+      })
+      .catch(err => {
+        res
+          .status(500)
+          .json({ error: err.stack });
+      });
   });
 
   return router;
